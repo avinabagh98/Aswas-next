adaafeed6f9e508ff5ad42e36239206b25b82959
Avinaba Ghosh <75163712+avinabagh98@users.noreply.github.com>
2024-02-10 20:09:54 +0530

    Base folder structure created

---
+++ aswas/app/home/layout.js
@@ -0,0 +1,10 @@
+
+import styles from './home.module.css';
+
+export default function homelayout({children}){
+      return(
+            <>    master layout
+                  {children}
+            </>
+      )
+}
