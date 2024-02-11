ea36810d44bd3cd0b42560b72dd054a5c79442fe
Avinaba Ghosh <75163712+avinabagh98@users.noreply.github.com>
2024-02-11 14:47:23 +0530

    button group created

--- aswas/components/choice/Buttongroup.js
+++ aswas/components/choice/Buttongroup.js
@@ -6,31 +6,35 @@ import { Row, Col } from "react-bootstrap";
 export default function Buttongroup() {
   return (
     <>
-      {/* <div className={styles.container}>
-        <div className={styles.btnContainer}>
-          <button className={styles.btn}>button1</button>
-        </div>
-      </div> */}
-
       <div className={styles.container}>
         <Row className={styles.btnContainer}>
-          <Col xs={4} className={styles.btn}>
-            <button>button1</button>
+          <Col xs={4} className={styles.btnCol}>
+            <span className={styles.btnSpan}>
+              <button>VCMO</button>
+            </span>
           </Col>
 
-          <Col xs={4} className={styles.btn}>
-            <button>button1</button>
+          <Col xs={4} className={styles.btnCol}>
+            <span className={styles.btnSpan}>
+              <button>HTH SUPERVISOR</button>
+            </span>
           </Col>
-          <Col xs={4} className={styles.btn}>
-            <button>button1</button>
+          <Col xs={4} className={styles.btnCol}>
+            <span className={styles.btnSpan}>
+              <button>HTH MEMBER</button>
+            </span>
           </Col>
 
           <Row className={styles.btnContainer}>
-            <Col xs={6} className={styles.btn}>
-              <button>button1</button>
+            <Col xs={6} className={styles.btnCol}>
+              <span className={styles.btnSpan}>
+                <button>VCT SUPERVISOR</button>
+              </span>
             </Col>
-            <Col xs={6} className={styles.btn}>
-              <button>button1</button>
+            <Col xs={6} className={styles.btnCol}>
+              <span className={styles.btnSpan}>
+                <button>VCT MEMBER</button>
+              </span>
             </Col>
           </Row>
         </Row>
