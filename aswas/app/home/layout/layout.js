afccf13632d177af0e819e2bf8223b319495ed4c
Avinaba Ghosh <75163712+avinabagh98@users.noreply.github.com>
2024-02-12 21:54:53 +0530

    survey form created

--- aswas/app/home/layout/layout.js
+++ aswas/app/home/layout/layout.js
@@ -1,10 +1,18 @@
 import styles from "./layout.module.css";
+import Textparser from "@/components/home/Textparser";
 
 export default function layout({ children }) {
   return (
     <>
       <div className={styles.container}>
-        this is under home / layout
+        <div className={styles.namebar}>
+          <span>
+            <Textparser text={"Arun Naskar (HTH)"} />
+            <br />
+            <Textparser text={"Baruipur-Ward No.12-Team No.2"} />
+          </span>
+          <span>Round-1</span>
+        </div>
         {children}
       </div>
     </>
--- aswas/app/home/layout/survey/page.js
+++ aswas/app/home/layout/survey/page.js
@@ -2,13 +2,44 @@
 import React from "react";
 import Surveyques from "@/components/home/Surveyques";
 import Surveyoption from "@/components/home/Surveyoption";
+import styles from "./survey.module.css";
+import Textparser from "@/components/home/Textparser";
+import { Button } from "react-bootstrap";
 
 export default function page() {
   return (
-    <div>
-      this is survey page
-      <Surveyques labelText="this is a label you can change through props dynamically" />
-      <Surveyoption optionText={"This is dynamically passed optionText"} />
+    <div className={styles.container}>
+      <div className={styles.titlebar}>
+        <span>
+          <Textparser text={"Form-No-2"} />
+        </span>
+        <span>
+          <Textparser text={"Round-2"} />
+        </span>
+      </div>
+      <div className={styles.content}>
+        <span>
+          <Surveyques labelText="this is a label you can change through props dynamically" />
+          <Surveyoption
+            optionText={"This is dynamically passed optionText gdgdgdrgdr"}
+          />
+          <Surveyoption
+            optionText={"This is dynamically passed optionText gdgdgdrgdr"}
+          />
+          <Surveyoption
+            optionText={"This is dynamically passed optionText gdgdgdrgdr"}
+          />
+          <Surveyques labelText="this is a label you can change through props dynamically" />
+          <Surveyques labelText="this is a label you can change through props dynamically" />
+          <Surveyques labelText="this is a label you can change through props dynamically" />
+          <Surveyques labelText="this is a label you can change through props dynamically" />
+        </span>
+        <div className={styles.imgContainer}>
+          <Textparser text={"Take Picture of the waste"} />
+          <img src="/images/camera_icon_to_upload.png"></img>
+        </div>
+        <Button variant="success"> Submit</Button>
+      </div>
     </div>
   );
 }
