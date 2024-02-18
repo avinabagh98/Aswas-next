4197952e8ca36570d3a48ad2b3a112676c31a679
Avinaba Ghosh <75163712+avinabagh98@users.noreply.github.com>
2024-02-19 01:19:56 +0530

    image capture added

--- aswas/app/home/(routes)/(with-routes-layout)/survey/page.js
+++ aswas/app/home/(routes)/(with-routes-layout)/survey/page.js
@@ -1,13 +1,39 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Surveyques from "@/components/home/Surveyques";
 import Surveyoption from "@/components/home/Surveyoption";
 import styles from "./survey.module.css";
 import Textparser from "@/components/home/Textparser";
 import { Button } from "react-bootstrap";
 
+
 export default function page() {
+
+  const [cameraClicked, setCameraClicked] = useState(false);
+  const [captureClicked, setCaptureClicked] = useState(false);
+
+
   const userRole = localStorage.getItem("role");
+  const camera_button = async () => {
+    try {
+      setCameraClicked(true);
+      let stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
+      video.srcObject = stream;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const click_button = () => {
+    try {
+      setCaptureClicked(true);
+      canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+      let image_data_url = canvas.toDataURL('image/jpeg');
+      console.log(image_data_url);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   return userRole === "hth" ? (
     <div className={styles.container}>
@@ -122,9 +148,15 @@ export default function page() {
           </span>
           <div className={styles.imgContainer}>
             <Textparser text={"Take Picture of the waste"} />
-            <img src="/images/camera_icon_to_upload.png"></img>
+            <a onClick={camera_button}><img src="/images/camera_icon_to_upload.png"></img></a>
           </div>
-          <Button variant="success" href="/home/layout/team">
+          {cameraClicked ? <div className="d-flex flex-column justify-content-center align-items-center" >
+            <video id="video" width="320" height="240" autoPlay></video>
+            <Button onClick={click_button}>Click Photo</Button>
+            <canvas id="canvas" width="320" height="240"></canvas>
+          </div> : <></>}
+
+          <Button variant="success" href="/home/team">
             Submit
           </Button>
         </div>
